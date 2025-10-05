969b3702162b22cbe647a1504f58bdebe8408a81
Shiv Kant <shivkant639624@gmail.com>
2025-10-05 18:57:41 +0530

    storage collection model code done

---
+++ src/models/server/storage.collection.ts
@@ -0,0 +1,32 @@
+import { storage } from "./config";
+import { questionAttachmentBucket } from "../name";
+import { Permission } from "node-appwrite";
+
+export default async function getOrCreateStorage() {
+  try {
+    await storage.getBucket(questionAttachmentBucket);
+    console.log("Storage Connected");
+  } catch (error) {
+    try {
+      await storage.createBucket(
+        questionAttachmentBucket,
+        questionAttachmentBucket,
+        [
+          Permission.create("users"),
+          Permission.read("any"),
+          Permission.read("users"),
+          Permission.update("users"),
+          Permission.delete("users"),
+        ],
+        false,
+        undefined,
+        undefined,
+        ["jpg", "png", "gif", "jpeg", "webp", "heic"]
+      );
+      console.log("Storage Created");
+      console.log("Storage Connected");
+    } catch (error) {
+      console.error("Error creating storage:", error);
+    }
+  }
+}
