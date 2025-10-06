49c33c59ba6875f6aa3040aa265737631626bfe8
Shiv Kant <shivkant639624@gmail.com>
2025-10-06 14:16:09 +0530

    (auth) layout completed

--- src/(auth)/layout.tsx
+++ src/(auth)/layout.tsx
@@ -1,7 +1,26 @@
+import { useAuthStore } from "@/store/Auth";
+import { useRouter } from "next/router";
 import React from "react";
 
-function layout() {
-  return <div>layout</div>;
-}
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useAuthStore();
+  const router = useRouter();
 
-export default layout;
+  React.useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [session, router]);
+
+  if (session) {
+    return null;
+  }
+
+  return (
+    <div className="">
+      <div className="">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
